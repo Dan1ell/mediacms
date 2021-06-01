1d6346015fc5ce6a9376566c329c7832bf0dc3a9
Dan1ell <dstrijdhaftig@gmail.com>
2021-06-01 17:34:47 -0300

    Prevent scrolling when popup is open.

--- static/js/popup.js
+++ static/js/popup.js
@@ -2,6 +2,7 @@
 var modal = null;
 var closeBtn = null;
 var openBtn = null;
+var body = document.getElementsByTagName("BODY")[0];
 
 checkElement('.bg-modal').then((element1) => {
     modal = element1
@@ -9,12 +10,14 @@ checkElement('.bg-modal').then((element1) => {
         closeBtn = element2
         closeBtn.addEventListener('click', () => {
         modal.style.display ='none'
+        body.style.overflow = 'visible' 
         })
     })		
     checkElement('.newsBtn').then((element3) => {
         openBtn = element3	
         openBtn.addEventListener('click', () => {
         modal.style.display ='flex'
+        body.style.overflow = 'hidden' 
         })					
     })
 })	
